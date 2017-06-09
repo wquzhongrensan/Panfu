6d579606a8eb2ac91dbe45205b07c6c7c14afaac
jianpingli <956023769@qq.com>
2017-06-09 14:13:42 +0800

    初始化项目

---
+++ Panfu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Panfu
+//
+//  Created by AppsComm on 2017/6/9.
+//  Copyright © 2017年 quzhongrensan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Panfu/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Panfu
+//
+//  Created by AppsComm on 2017/6/9.
+//  Copyright © 2017年 quzhongrensan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
